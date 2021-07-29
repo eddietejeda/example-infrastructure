337e1140c6e1a57dc7f2de4ba0e1efb490c69807
Eddie A Tejeda <eddie.tejeda@gmail.com>
2021-07-29 17:52:51 -0400

    organize var names

--- variables.tf
+++ variables.tf
@@ -1,29 +1,20 @@
+# App
 variable "name" {
   default     = "linkbird"
   description = "The name of the application"
 }
 
-
-
-# Code build
-variable "codebuild_buildspec_path" {}
-
-
-variable "git_branch" {
-  default = "main"
+variable "environment" {
+  description = "Examples: development, staging, production"
 }
 
-variable "codebuild_timeout" {
-  default = 5
-}
+# variable  "app_key" {} 
+variable  "app_encryption_key" {} 
+
 
 # AWS
 variable "region" {
   description = "The default region"
-}
-
-variable "environment" {
-  description = "Examples: development, staging, production"
 }
 
 variable "primary_domain" {
@@ -34,46 +25,28 @@ variable "secondary_domain" {
   description = "The secondary domain name"
 }
 
-variable "codebuild_image" {
-  default = "linkbird:latest"
-}
-
 variable "bucket_name" {
   description = "The name of the private bucket"
 }
 
-variable "private_bucket_name" {
-  description = "The name of the private bucket"
-}
-
-# variable  "app_key" {} 
-variable  "app_encryption_key" {} 
-
 
 # Github 
-variable "github_repo" {
-  description = "Default Github repo"
-}
-
-variable "github_token" {
-  description = "Github token"
-}
-
+variable "github_repo" {}
+variable "github_token" {}
+variable "github_webhook_secret" {} 
 variable "github_webhook_token" {}
-
 variable "git_organization" {}
-
 variable "github_repository" {}
-
 variable "github_url" {}
+variable "git_branch" {
+  default = "main"
+}
 
 # Dockerhub
 variable "dockerhub_username" {}
-
 variable "dockerhub_access_token" {}
 
-variable  "github_webhook_secret" {} 
-
+# New Relic
 variable  "new_relic_license_key" {} 
 
 # Twitter tokens
@@ -88,8 +61,14 @@ variable  "twitter_worker_access_token_secret" {}
 variable  "twitter_worker_consumer_key" {}  
 variable  "twitter_worker_consumer_secret" {} 
 
-
+# Stripe
 variable  "stripe_price_key" {} 
 variable  "stripe_product_key" {} 
 variable  "stripe_publishable_key" {}
 variable  "stripe_secret_key" {}
+
+# Code build
+variable "codebuild_buildspec_path" {}
+variable "codebuild_timeout" {}
+variable "codebuild_image" {}
+
