61782cc3061b0d1c6552a8f87e6ac8d72fbe74ca
Eddie A Tejeda <eddie.tejeda@gmail.com>
2021-08-01 07:53:33 -0400

    Replace references to lb with alb

--- acm.tf
+++ acm.tf
@@ -29,7 +29,7 @@ resource "aws_acm_certificate_validation" "primary_certificate_validation" {
   certificate_arn         = "${aws_acm_certificate.primary_cert.arn}"
   validation_record_fqdns = [for record in aws_route53_record.primary_cert_validation : record.fqdn]
 
-  # Optional: Hook to add delay before aws_lb_listener_certificate.lb_listener_certificate
+  # Optional: Hook to add delay before aws_alb_listener_certificate.lb_listener_certificate
   provisioner "local-exec" {
     command = "sleep 20"
   }
@@ -45,8 +45,8 @@ resource "aws_acm_certificate_validation" "secondary_certificate_validation" {
   }
 }
 
-resource "aws_lb_listener_certificate" "lb_listener_certificate" {
-  listener_arn    = aws_lb_listener.https_lb_listener.arn
+resource "aws_alb_listener_certificate" "lb_listener_certificate" {
+  listener_arn    = aws_alb_listener.https_lb_listener.arn
   certificate_arn = aws_acm_certificate.primary_cert.arn
   
   depends_on = [
@@ -54,4 +54,3 @@ resource "aws_lb_listener_certificate" "lb_listener_certificate" {
   ]
 
 }
-
